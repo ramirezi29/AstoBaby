a8d1875bbd154d253b016c7702ce2ec4b9330c7e
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2019-03-19 18:01:23 -0600

    Contains compatability chart Sets

---
+++ AstroBaby/Utilities/ZodiacEnum.swift
@@ -0,0 +1,24 @@
+//
+//  ZodiacEnum.swift
+//  AstroBaby
+//
+//  Created by Ivan Ramirez on 3/19/19.
+//  Copyright © 2019 ramcomw. All rights reserved.
+//
+
+import Foundation
+
+enum Zoo: String {
+    case taurus = "Taurus"
+    case aries = "aries"
+    case gemini = "gemini"
+    case cancer = "cancer"
+    case leo = "leo"
+    case virgo = "virgo"
+    case libra = "libra"
+    case scorpio = "scorpio"
+    case sagittarius = "sagittarius"
+    case capricorn = "capricorn"
+    case aquarius = "aquarius"
+    case pisces = "pisces"
+}
